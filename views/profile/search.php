099e87358db8bd2bfef6cac504dfbc7db3fa40d6
SonicLighter <pashkevich.s.d@yandex.ru>
2016-07-07 13:03:54 +0300

    update070716-1303

--- views/profile/search.php
+++ views/profile/search.php
@@ -17,52 +17,53 @@ $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
-    <p>
-         <?= Html::a('Reset search', [Url::toRoute(['search'])], ['class' => 'btn btn-info']) ?>
-    </p>
-    <!--<?=
-          ListView::widget([
-               'dataProvider' => $dataProvider,
-               'summary' => false,
-               'itemView' => 'search/user',
-          ]);
-    ?>-->
-    <?=
-         GridView::widget([
-              'dataProvider' => $dataProvider,
-              'filterModel' => $searchModel,
-              'summary' => false,
-              'columns' => [
-                  [
-                       'class' => DataColumn::className(), // this line is optional
-                       'attribute' => 'username',
-                       'label' => 'Search by username',
-                       'format' => 'html',
-                       //'header' => 'Name',
-                       'value' => function($model){
-                            $resultString =
-                                "<div class='searchWrapper'>
-                                      <div class='searchLeftColumn'>
-                                           ".Html::img(Url::toRoute($model->profilePicture), ['width' => '120px'])."
-                                      </div>
-                                      <div class='searchMiddleColumn'>
-                                           <h4>Username: ".HtmlPurifier::process($model->username)."</h4>
+     <div class='searchPage'>
+         <h1><?= Html::encode($this->title) ?></h1>
+         <p>
+              <?= Html::a('Reset search', [Url::toRoute(['search'])], ['class' => 'btn btn-info']) ?>
+         </p>
+         <!--<?=
+               ListView::widget([
+                    'dataProvider' => $dataProvider,
+                    'summary' => false,
+                    'itemView' => 'search/user',
+               ]);
+         ?>-->
+         <?=
+              GridView::widget([
+                   'dataProvider' => $dataProvider,
+                   'filterModel' => $searchModel,
+                   'summary' => false,
+                   'columns' => [
+                       [
+                            'class' => DataColumn::className(), // this line is optional
+                            'attribute' => 'username',
+                            'label' => 'Search by username',
+                            'format' => 'html',
+                            //'header' => 'Name',
+                            'value' => function($model){
+                                 $resultString =
+                                     "<div class='searchWrapper'>
+                                           <div class='searchLeftColumn'>
+                                                ".Html::img(Url::toRoute($model->profilePicture), ['width' => '120px'])."
+                                           </div>
+                                           <div class='searchMiddleColumn'>
+                                                <h4>Username: ".HtmlPurifier::process($model->username)."</h4>
 
-                                           E-mail: ".HtmlPurifier::process($model->email)."<br/>
+                                                E-mail: ".HtmlPurifier::process($model->email)."<br/>
 
-                                           Role: ".HtmlPurifier::process($model->role->item_name)."
+                                                Role: ".HtmlPurifier::process($model->role->item_name)."
 
-                                      </div>
-                                      <div class='searchRightColumn'>
-                                           ".Html::a('Add to friends', [Url::toRoute(['invite', 'id' => $model->id])], ['class' => 'btn btn-info'])."
-                                      </div>
-                                 </div>";
-                                 return $resultString;
-                       },
-                  ],
-              ],
-         ]);
-     ?>
-
+                                           </div>
+                                           <div class='searchRightColumn'>
+                                                ".Html::a('Add to friends', [Url::toRoute(['invite', 'id' => $model->id])], ['class' => 'btn btn-info'])."
+                                           </div>
+                                      </div>";
+                                      return $resultString;
+                            },
+                       ],
+                   ],
+              ]);
+          ?>
+     </div>
 </div>
